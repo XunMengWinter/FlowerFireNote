e7e4b1c70f87768b41aee76006d22cc9ee692892
ice <XunMengWinter@gmail.com>
2026-06-12 23:24:12 +0800

    Initial Commit

---
+++ FlowerFireNote3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowerFireNote3
+//
+//  Created by ice on 12/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowerFireNote3/FlowerFireNote3App.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerFireNote3App.swift
+//  FlowerFireNote3
+//
+//  Created by ice on 12/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FlowerFireNote3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
